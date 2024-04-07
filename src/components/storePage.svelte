348aa90553cddeb6f19e84cc2b6d413c4f3fc9b1
UnwntdEngr <158253674+UnwntdEngr@users.noreply.github.com>
2024-04-07 14:48:13 +0800

    Update storePage.svelte

--- src/components/storePage.svelte
+++ src/components/storePage.svelte
@@ -2,14 +2,12 @@
   import { myCart } from '../store/myOrder';
 export let name;
 export let price;
-export let category;
 export let id;
 </script>
 
 <div class="dessert-card">
   <h2>{name}</h2>
   <p class="dessert-price">₱ {price}</p>
-  <p class="product-category">Category: {category}</p>
   <button 
   class="btn add-to-cart-btn" on:click={()=>$myCart=[...$myCart,{id,name,price}]}>Add to cart
 </button>
